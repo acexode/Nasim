3a33fe97712d6665cf2669245cab0d113a3c1228
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-09-13 11:36:31 +0100

    Fixed class names

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,25 +4,25 @@ import './nav.scss';
 
 const Nav = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3 navbar-scrolled" id="mainNav">
-            <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <nav classname="navbar navbar-expand-lg navbar-light fixed-top py-3 navbar-scrolled" id="mainNav">
+            <div classname="container">
+                <a classname="navbar-brand js-scroll-trigger" href="#page-top">
                     <img src={logo} alt="" />
                 </a>
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse bg-white pl-3 mx-n3" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto my-2 my-lg-0 align-items-center">
-                        <li class="nav-item">
-                            <a class="nav-link js-scroll-trigger" href="#home">Home</a>
+                <button classname="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span classname="navbar-toggler-icon"></span></button>
+                <div classname="collapse navbar-collapse bg-white pl-3 mx-n3" id="navbarResponsive">
+                    <ul classname="navbar-nav ml-auto my-2 my-lg-0 align-items-center">
+                        <li classname="nav-item">
+                            <a classname="nav-link js-scroll-trigger" href="#home">Home</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link js-scroll-trigger" href="#apply">Apply</a>
+                        <li classname="nav-item">
+                            <a classname="nav-link js-scroll-trigger" href="#apply">Apply</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+                        <li classname="nav-item">
+                            <a classname="nav-link js-scroll-trigger" href="#about">About</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+                        <li classname="nav-item">
+                            <a classname="nav-link js-scroll-trigger" href="#contact">Contact</a>
                         </li>
                         <li className="nav-item">
                             <a className="btn btn--outlined px-5 m-2" href="login">Login</a>
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -16,13 +16,13 @@ const Footer = () => {
           </div>
           <div className="my-2 text-center col-12 col-md-2">
             <a href="#">
-              <i class="fa fa-facebook" aria-hidden="true"></i>
+              <i classname="fa fa-facebook" aria-hidden="true"></i>
             </a>
             <a href="#">
-              <i class="fa fa-twitter" aria-hidden="true"></i>
+              <i classname="fa fa-twitter" aria-hidden="true"></i>
             </a>
             <a href="#">
-              <i class="fa fa-instagram" aria-hidden="true"></i>
+              <i classname="fa fa-instagram" aria-hidden="true"></i>
             </a>
           </div>
         </div>
