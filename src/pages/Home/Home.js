18373a05ce50c9679ddc8d73f715257e6d6206ef
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-09-13 11:39:40 +0100

    Fixed class names

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -63,14 +63,14 @@ export const Home = () => {
                         } else {
                             customClass = 'hero-item hero row ml-0'
                         }
-                        return <HeroItem key={index} data={item} cClassName={customClass} />
+                        return <HeroItem key={index} data={item} cClass={customClass} />
                     })}
                     <div className='hero__ctrls row'>
                         <button className='m-1 hero__ctrls__left' onClick={moveLeft} disabled={count === 0}>
-                            <i classname='fa fa-angle-left' aria-hidden='true'></i>
+                            <i className='fa fa-angle-left' aria-hidden='true'></i>
                         </button>
                         <button className='m-1 hero__ctrls__right' onClick={moveRight} disabled={count === 2}>
-                            <i classname='fa fa-angle-right' aria-hidden='true'></i>
+                            <i className='fa fa-angle-right' aria-hidden='true'></i>
                         </button>
                     </div>
                     <div className="banner py-4">
@@ -85,11 +85,11 @@ export const Home = () => {
                         <div className="row justify-content-between align-items-center">
                             <div className="col-12 col-md-6 about__text">
                                 <p className="about__tag">A little about us</p>
-                                <h2 classname="font-weight-bold">
+                                <h2 className="font-weight-bold">
                                     Providing efficient, effective, and coordinated
                                     humanitarian response to Nigerians.
                                 </h2>
-                                <p classname="lighten my-5">
+                                <p className="lighten my-5">
                                 In order to guarantee high-level political authority
                                 and coordination for all social protection programmes
                                 within Nigeria, the Federal Government of Nigeria has
@@ -99,7 +99,7 @@ export const Home = () => {
                                 life of dignity for those who have been constrained,
                                 in one way or another, from achieving their full potential.
                                 </p>
-                                <a href="/apply" classname="btn btn--primary px-5 py-3">Read More</a>
+                                <a href="/apply" className="btn btn--primary px-5 py-3">Read More</a>
                             </div>
                             <div className="col-12 col-md-6 about__image">
                                 <img src={aboutImage} alt="About image" />
@@ -109,17 +109,17 @@ export const Home = () => {
                 </section>
                 <section className="programmes">
                     <div className="container">
-                        <h2 classname="text-center font-weight-bold">Our Programmes</h2>
-                        <p classname="text-center lighten mx-auto my-3">
+                        <h2 className="text-center font-weight-bold">Our Programmes</h2>
+                        <p className="text-center lighten mx-auto my-3">
                             Our programmes are specifically designed to empower, train and
                             uplift millions of abandoned Nigerians in every part of the country.
                         </p>
                         <div className="row programs my-5 mx-0">
                             <div className="col-12 col-md-6 program px-0 my-3">
-                                <div classname="program__image n-power"></div>
-                                <div classname="p-4">
-                                    <h3 classname="program__name font-weight-bold">N-POWER</h3>
-                                    <p classname="lighten mt-3 mb-5">
+                                <div className="program__image n-power"></div>
+                                <div className="p-4">
+                                    <h3 className="program__name font-weight-bold">N-POWER</h3>
+                                    <p className="lighten mt-3 mb-5">
                                         The N-Power Programme is a job creation and
                                         skills empowerment programme of the Federal
                                         Government of Nigeria. N-Power is designed
@@ -130,14 +130,14 @@ export const Home = () => {
                                         and global markets. The N-Power programme is
                                         for young Nigerians between the ages of 18 and 35.
                                     </p>
-                                    <a href="/apply" classname="btn btn--primary px-5 py-3">Apply Now</a>
+                                    <a href="/apply" className="btn btn--primary px-5 py-3">Apply Now</a>
                                 </div>
                             </div>
                             <div className="col-12 col-md-6 program px-0 my-3">
-                                <div classname="program__image trader"></div>
-                                <div classname="p-4">
-                                    <h3 classname="program__name font-weight-bold">Trader Moni</h3>
-                                    <p classname="lighten mt-3 mb-5">
+                                <div className="program__image trader"></div>
+                                <div className="p-4">
+                                    <h3 className="program__name font-weight-bold">Trader Moni</h3>
+                                    <p className="lighten mt-3 mb-5">
                                         TraderMoni is a loan programme of the Federal
                                         Government, created specifically for petty
                                         traders and artisans across Nigeria. It is a
@@ -148,7 +148,7 @@ export const Home = () => {
                                         N10,000 and growing all the way to N100,000 as
                                         you pay back. 
                                     </p>
-                                    <a href="/apply" classname="btn btn--primary px-5 py-3">Apply Now</a>
+                                    <a href="/apply" className="btn btn--primary px-5 py-3">Apply Now</a>
                                 </div>
                             </div>
                         </div>
@@ -156,18 +156,18 @@ export const Home = () => {
                 </section>
                 <section className="partners">
                     <div className="container">
-                        <h2 classname="text-center font-weight-bold">Our Partners</h2>
-                        <div classname="row my-5">
-                            <div classname="partner col">
+                        <h2 className="text-center font-weight-bold">Our Partners</h2>
+                        <div className="row my-5">
+                            <div className="partner col">
                                 <img src="/bank.svg" alt="Bank icon" />
                             </div>
-                            <div classname="partner col">
+                            <div className="partner col">
                                 <img src="/cbn.svg" alt="CBN icon" />
                             </div>
-                            <div classname="partner col">
+                            <div className="partner col">
                                 <img src="/dangote.svg" alt="Dangote icon" />
                             </div>
-                            <div classname="partner col">
+                            <div className="partner col">
                                 <img src="/nassarawa.svg" alt="Nassarawa icon" />
                             </div>
                         </div>
@@ -175,65 +175,65 @@ export const Home = () => {
                 </section>
                 <section className="news-section">
                     <div className="container">
-                        <h2 classname="text-center font-weight-bold">Latest News</h2>
-                        <p classname="text-center lighten mx-auto my-3">
+                        <h2 className="text-center font-weight-bold">Latest News</h2>
+                        <p className="text-center lighten mx-auto my-3">
                             Keep up with the latest updates about programmes and events
                         </p>
                         <div className="row news my-5 mx-0">
                             <div className="col-12 col-md-4 item px-0 my-2">
-                                <div classname="item__image"></div>
-                                <div classname="p-4 pb-0">
-                                    <h3 classname="item__name font-weight-bold">
+                                <div className="item__image"></div>
+                                <div className="p-4 pb-0">
+                                    <h3 className="item__name font-weight-bold">
                                         Impacting Lives During A Global Pandemic
                                     </h3>
-                                    <p classname="item__date">
+                                    <p className="item__date">
                                         June 25, 2020
                                     </p>
-                                    <p classname="lighten my-3">
+                                    <p className="lighten my-3">
                                         The N-Power Programme is a job creation
                                         and skills empowerment programme of the
                                         Federal Government of Nigeria…
                                     </p>
                                 </div>
-                                <div classname="item__action">
+                                <div className="item__action">
                                     Read More
                                 </div>
                             </div>
                             <div className="col-12 col-md-4 item px-0 my-2">
-                                <div classname="item__image"></div>
-                                <div classname="p-4 pb-0">
-                                    <h3 classname="item__name font-weight-bold">
+                                <div className="item__image"></div>
+                                <div className="p-4 pb-0">
+                                    <h3 className="item__name font-weight-bold">
                                         Impacting Lives During A Global Pandemic
                                     </h3>
-                                    <p classname="item__date">
+                                    <p className="item__date">
                                         June 25, 2020
                                     </p>
-                                    <p classname="lighten my-3">
+                                    <p className="lighten my-3">
                                         The N-Power Programme is a job creation
                                         and skills empowerment programme of the
                                         Federal Government of Nigeria…
                                     </p>
                                 </div>
-                                <div classname="item__action">
+                                <div className="item__action">
                                     Read More
                                 </div>
                             </div>
                             <div className="col-12 col-md-4 item px-0 my-2">
-                                <div classname="item__image"></div>
-                                <div classname="p-4 pb-0">
-                                    <h3 classname="item__name font-weight-bold">
+                                <div className="item__image"></div>
+                                <div className="p-4 pb-0">
+                                    <h3 className="item__name font-weight-bold">
                                         Impacting Lives During A Global Pandemic
                                     </h3>
-                                    <p classname="item__date">
+                                    <p className="item__date">
                                         June 25, 2020
                                     </p>
-                                    <p classname="lighten my-3">
+                                    <p className="lighten my-3">
                                         The N-Power Programme is a job creation
                                         and skills empowerment programme of the
                                         Federal Government of Nigeria…
                                     </p>
                                 </div>
-                                <div classname="item__action">
+                                <div className="item__action">
                                     Read More
                                 </div>
                             </div>
